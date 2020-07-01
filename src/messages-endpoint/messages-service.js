0950765acdc5e4b991080276eae80557970424b4
Daniel Sottile <danielsottile@gmail.com>
2020-07-01 16:45:36 -0400

    fixed bug

--- src/messages-endpoint/messages-service.js
+++ src/messages-endpoint/messages-service.js
@@ -6,6 +6,7 @@ const MessagesService = {
       .from('messages')
       .select('*')
       .orderByRaw('random()')
+      .where('flagged', 'false')
       .limit(10);
   },
 
@@ -14,6 +15,7 @@ const MessagesService = {
       .from('messages')
       .select('*')
       .orderByRaw('random()')
+      .where('flagged', 'false')
       .limit(1);
   },
 
