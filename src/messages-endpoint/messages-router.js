8b94d0d13f3ea3a38a701f9aa40a46f89e0b9d8f
Joseph Rivera <antonibrivera@gmail.com>
2020-07-06 14:01:25 -0400

    quicksave

--- src/messages-endpoint/messages-router.js
+++ src/messages-endpoint/messages-router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const logger = require('../logger');
 const MessagesRouter = express.Router();
 const toxicity = require('@tensorflow-models/toxicity');
-const dataParser = express.json();
 const {requireAuth} = require('../middleware/jwt-auth');
 const MessagesService = require('./messages-service');
 
@@ -12,10 +11,10 @@ MessagesRouter
     MessagesService.getTenRandom(req.app.get('db'))
       .then(messages => res.json(messages.map(message => MessagesService.serialize(message))));
   })
-  .post(dataParser, requireAuth, (req, res, next) => {
+  .post([express.json(), requireAuth], (req, res, next) => {
     const { message } = req.body;
     const threshold = 0.85;
-
+    
     if(!message){
       return res.status(400).json({error: 'message must exist'});
     }
