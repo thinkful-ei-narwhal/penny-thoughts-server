916d206628067383c5251ce8c77d21668a1becb3
Cesar Morales <cesarlenin2@live.com>
2020-07-13 12:01:49 -0700

    finished testing  for all get calls

--- test/messages-endpoints.specs.js
+++ test/messages-endpoints.specs.js
@@ -25,7 +25,7 @@ describe('Messages Endpoints', function () {
   afterEach('cleanup', () => helpers.cleanTables(db));
 
   describe('GET /api/messages', () => {
-    context('Given no habits', () => {
+    context('Given no messages', () => {
       beforeEach(() => helpers.seedUsers(db, testUsers));
       it('responds with 200 and an empty list', () => {
         return supertest(app)
@@ -43,7 +43,6 @@ describe('Messages Endpoints', function () {
         )
       );
       it('responds with 200 and 10 messages', () => {
-
         return supertest(app)
           .get('/api/messages')
           .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
@@ -59,5 +58,122 @@ describe('Messages Endpoints', function () {
       });
     });
   });
+  //GET /api/messages
+
+  describe('/api/messages/single/:id', () => {
+    context('Given no habits', () => {
+      beforeEach(() => helpers.seedUsers(db, testUsers));
+      it('responds with 200 and an empty list', () => {
+        return supertest(app)
+          .get('/api/messages/single/1')
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(200, []);
+      });
+    });
+    context('Given there are messages in the database', () => {
+      beforeEach('insert messages', () =>
+        helpers.seedMessagesTables(
+          db,
+          testUsers,
+          testMessages
+        )
+      );
+      it('responds with 200 and 1 message', () => {
+        return supertest(app)
+          .get('/api/messages/single/1')
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(200)
+          .expect(res => {
+            expect(res.body).to.be.a('array');
+            expect(res.body).to.have.length(1);
+            res.body.forEach((item) => {
+              expect(item).to.be.a('object');
+              expect(item).to.include.keys('id', 'message', 'user_id', 'archived', 'date_created', 'date_modified', 'flagged');
+            });
+          });
+      });
+    });
+  });
+  //GET /api/messages/single/:id
+
+  describe('/api/messages/flagged', () => {
+    context('Given no messages', () => {
+      beforeEach(() => helpers.seedUsers(db, testUsers));
+      it('responds with 200 and an empty list', () => {
+        return supertest(app)
+          .get('/api/messages/flagged')
+          .set('Authorization', helpers.makeAuthHeader(testUsers[2]))
+          .expect(200, []);
+      });
+    });
+    context('Given there are messages in the database', () => {
+      beforeEach('insert messages', () =>
+        helpers.seedMessagesTables(
+          db,
+          testUsers,
+          testMessages
+        )
+      );
+      it('responds with 200 and 2 flagged message', () => {
+        const expectedMessages = testMessages.map(messages =>
+          helpers.makeExpectedMessages(messages)
+        );
+        const filterMessages = expectedMessages.filter(message => (message.flagged === true) && (message.archived=== false));
+
+        return supertest(app)
+          .get('/api/messages/flagged')
+          .set('Authorization', helpers.makeAuthHeader(testUsers[2]))
+          .expect(200, filterMessages);
+      });
+    });
+  });
+  //GET /api/messages/flagged
+
+  describe('/api/messages/userData', () => {
+    context('Given no messages', () => {
+      beforeEach(() => helpers.seedUsers(db, testUsers));
+      it('responds with 200 and an empty list', () => {
+        return supertest(app)
+          .get('/api/messages/userData')
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(200, []);
+      });
+    });
+    context('Given there are messages in the database', () => {
+      beforeEach('insert messages', () =>
+        helpers.seedMessagesTables(
+          db,
+          testUsers,
+          testMessages
+        )
+      );
+      it('responds with 200 and all user message', () => {
+        const expectedMessages = testMessages.map(messages =>
+          helpers.makeExpectedMessages(messages)
+        );
+        const filterMessages = expectedMessages.filter(message => message.user_id === 1);
+
+        return supertest(app)
+          .get('/api/messages/userData')
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(200, filterMessages);
+      });
+    });
+  });
+  //GET /api/messages/userData
+
+
 
 });
+// GET /api/messages *********
+//GET /api//messages/single/:id   *********
+//GET /api/messages/flagged   ******
+//GET /api/messages/userData
+
+//POST /api/messages
+
+//PATCH /api/messages/flagged
+//PATCH /api/messages/archive
+//PATCH /api/messages/userData
+//PATCH /api/messages/report
+//DELETE /api/messages/userData
